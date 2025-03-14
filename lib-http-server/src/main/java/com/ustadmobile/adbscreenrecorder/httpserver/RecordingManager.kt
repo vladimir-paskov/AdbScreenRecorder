0c1bd65a68cee7d2ad85b09b83dd98be6a534e18
Vladimir Paskov <vlado.paskov@gmail.com>
2025-03-15 01:34:41 +0200

    ScrCpy process management on Windows

--- lib-http-server/src/main/java/com/ustadmobile/adbscreenrecorder/httpserver/RecordingManager.kt
+++ lib-http-server/src/main/java/com/ustadmobile/adbscreenrecorder/httpserver/RecordingManager.kt
@@ -20,7 +20,7 @@ class RecordingManager(
     private val recordingType: RecordingType = RecordingType.ADB,
     private val adbPath: String,
     private val destDir: File,
-    private val scrcpyPath: String? = null) {
+    private val scrcpyPath: String? = null) { // TODO: Test behaviour where no scrcpy is available
 
     companion object {
         private const val MIN_SDK = 22
@@ -36,10 +36,52 @@ class RecordingManager(
                 "scrcpy"
             }
         }
+
+        private fun getScrCpyPid(): String? {
+            if(isWindows()) {
+                val processDetails = runProcess(
+                    listOf(
+                        "tasklist",
+                        "/FI", "IMAGENAME eq scrcpy.exe",
+                        "/FO", "CSV",
+                        "/NH"
+                    )
+                ).bufferedReader().readLines()
+
+                // Handle the case where there are no scrcpy tasks running
+                val filteredDetails = processDetails.filter { !it.startsWith("INFO:") }
+
+                val parsedProcesses = filteredDetails.mapNotNull { line ->
+                    val values = line.split(",").map { it.trim().removeSurrounding("\"") }
+                    if (values.size >= 2) {
+                        values[1]
+                    } else {
+                        null
+                    }
+                }
+
+                if(parsedProcesses.isEmpty()) {
+                    return null
+                }
+
+                for(pid in parsedProcesses) {
+                    println(pid)
+                }
+
+                // Assumption that PIDs are sorted by the order of starting
+                val pid = parsedProcesses[parsedProcesses.size - 1]
+                println("Pid is: $pid")
+                return pid
+            } else {
+                // TODO: Get nix PIDs
+                return ""
+            }
+        }
     }
 
-    data class ProcessHolder(val process: Process, val recordingType: RecordingType,
-                             val windowId: String? = null)
+    data class ProcessHolder(val process: Process,
+                             val recordingType: RecordingType,
+                             val pid: String? = null)
 
     private val recordings = mutableMapOf<String, ProcessHolder>()
 
@@ -107,7 +149,7 @@ class RecordingManager(
             .redirectError(ProcessBuilder.Redirect.PIPE)
             .start()
 
-        return ProcessHolder(recordProcess, RecordingType.SCRCPY)
+        return ProcessHolder(recordProcess, RecordingType.SCRCPY, getScrCpyPid())
     }
 
     fun stopRecording(deviceName: String, clazzName: String, testName: String): File {
@@ -144,8 +186,8 @@ class RecordingManager(
 
             recordings.remove("$clazzName-$testName")
         } else {
-            println("Attempting to stop 'ScrCpy'.")
-            stopProcess(getScrCpyProcessName())
+            println("Attempting to stop 'ScrCpy'. PID: ${processHolder.pid}")
+            stopProcess(processHolder.pid)
             println("Waiting for 'ScrCpy' to stop.")
             processHolder.process.waitFor(60, TimeUnit.SECONDS)
 
@@ -157,11 +199,24 @@ class RecordingManager(
         return destVideoFile
     }
 
-    private fun stopProcess(processName: String) {
-        if(isWindows()) {
-            runProcess(listOf("taskkill", "/IM", processName, "/T"))
+    private fun stopProcess(processId: String?) {
+        if(processId.isNullOrEmpty()) {
+            // If we have no process id, kill all scrcpy instances
+            if (isWindows()) {
+                runProcess(listOf("taskkill", "/F", "/IM", getScrCpyProcessName()))
+            } else {
+                runProcess(listOf("pkill", "-SIGINT", getScrCpyProcessName()))
+            }
         } else {
-            runProcess(listOf("pkill", "-SIGINT", processName))
+            if(isWindows()) {
+                // Using windows-kill to send Ctrl - C to ScrCpy, because otherwise
+                // the recording will be corrupted.
+                // https://github.com/ElyDotDev/windows-kill
+                runProcess(listOf("windows-kill", "-2", processId))
+            } else {
+                // TODO: This should kill by PID also
+                runProcess(listOf("pkill", "-SIGINT", processId))
+            }
         }
     }
 }
